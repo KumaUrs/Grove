d8ff245749061f3a641138ecd0baf1f1991fdb61
rfjax <rfjax@LAPTOP-FKA2615U>
2021-02-16 20:18:47 -0500

    First commit

---
+++ Grove/src/EntryGrove.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author rfjax
+ *
+ */
+public class EntryGrove {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
