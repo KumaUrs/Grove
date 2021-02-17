cf8ce85a1691a2097017e90b195f17ab2b669235
Felix <Felix@192.168.0.93>
2021-02-17 08:09:49 -0500

    Discard/Draw card logic

--- Grove/src/PlayerDeck/DeckController.java
+++ Grove/src/PlayerDeck/DeckController.java
@@ -12,6 +12,11 @@ public class DeckController
 		drawHand();
 	}
 
+	private byte waterAffinityMultiplier;
+	private byte fireAffinityMultiplier;
+	private byte airAffinityMultiplier;
+	private byte earthAffinityMultiplier;
+	
 	private ArrayList<Card> playerDeck = new ArrayList<>();
 	private ArrayList<Card> playerHand = new ArrayList<>();
 	private ArrayList<Card> playerGraveyard = new ArrayList<>();
@@ -21,11 +26,42 @@ public class DeckController
 		Collections.shuffle(playerDeck);
 	}
 	
+	/**
+	 * Creates a temporary local variable of type card to save on one search loop
+	 * removes the first card on top of the deck.
+	 * adds the temporary card to players hand.
+	 * Adds a tick to affinityMultiplier of the specific elemental type.
+	 */
 	public void drawCard() 
 	{
-		playerHand.add(playerDeck.get(playerDeck.size()-1));
-		playerDeck.remove(playerDeck.size()-1);
+		// Temp card to save 1 search algorithm
+		Card tempCard = playerDeck.get(playerDeck.size());
+		playerDeck.remove(playerDeck.size());
+		
+		playerHand.add(tempCard);
+		affinityMultiplier(tempCard.elementalType);
+		
+		
 		shuffleDeck();
+	}
+	
+	public void affinityMultiplier(Element ele)
+	{
+		switch(ele) 
+		{
+		case AIR:
+			airAffinityMultiplier++;
+			break;
+		case WATER:
+			waterAffinityMultiplier++;
+			break;
+		case EARTH:
+			earthAffinityMultiplier++;
+			break;
+		case FIRE:
+			fireAffinityMultiplier++;
+			break;
+		}
 	}
 	
 	public void drawHand()
@@ -36,11 +72,21 @@ public class DeckController
 		}
 	}
 	
-	public void discardCard()
+	/**
+	 * Adjusts elemental multiplier.
+	 * Adds player card to grave yard.
+	 * Removes card from players hand.
+	 * @param card
+	 * @param cardIndex
+	 */
+	public void discardCard(Card card, byte cardIndex)
 	{
-		playerGraveyard.add(playerDeck.get(playerDeck.size()-1));
-		playerDeck.remove(playerDeck.size()-1);
-		shuffleDeck();
+		affinityMultiplier(card.elementalType);
+		
+		playerGraveyard.add(card);
+		
+		playerHand.remove(cardIndex);
+		
 	}
 
 	public ArrayList<Card> getPlayerDeck()
@@ -48,6 +94,10 @@ public class DeckController
 		return playerDeck;
 	}
 
+	
+	/**
+	 * Creates 52 objects of type card, then places them into the player deck.
+	 */
 	private void createPlayerDeck() 
 	{
 		for(int i = 0; i > 51; i++)
